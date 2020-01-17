713878c5530f277f600aaa1de03a69172e7f5543
zhaoruihang <18829509099@163.com>
2020-01-17 14:11:52 +0800

    IDEA整合GitHub测试

---
+++ day15_Servlet&HTTP&Request/src/cn/jiuwei/web/Servlet/ServletDemo2.java
@@ -0,0 +1,21 @@
+package cn.jiuwei.web.Servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo2")
+public class ServletDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet....");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost....");
+    }
+}
---
+++ day15_Servlet&HTTP&Request/src/cn/jiuwei/web/request/RequestDemo2.java
@@ -0,0 +1,21 @@
+package cn.jiuwei.web.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/requestDemo2")
+public class RequestDemo2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //演示获取请求头数据
+
+        //1.获取所有的请求头名称
+    }
+}
